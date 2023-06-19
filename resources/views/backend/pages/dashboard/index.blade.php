035c911668f60620c463030310e30b705fa61343
jaime <jaime@Ubuntu.myguest.virtualbox.org>
2023-06-19 03:01:13 -0700

    adding style

--- resources/views/backend/pages/dashboard/index.blade.php
+++ resources/views/backend/pages/dashboard/index.blade.php
@@ -103,7 +103,7 @@
     <div class="col">
       <div class="card bg-default shadow">
         <div class="card-header bg-transparent border-0">
-          <h3 class="text-white mb-0">Dark table</h3>
+          <h3 class=" mb-0">Dark table</h3>
         </div>
         <div class="table-responsive">
           <table class="table align-items-center table-dark table-flush">
