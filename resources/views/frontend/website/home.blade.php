f734e9c94b788e9ad5fd06f1822214c89f519339
jaime <jaime@Ubuntu.myguest.virtualbox.org>
2023-07-04 05:28:55 -0700

    home view added

--- resources/views/frontend/website/home.blade.php
+++ resources/views/frontend/website/home.blade.php
@@ -6,9 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name') }} | Home</title>
     <link rel="icon" href="{{ asset("frontend/assets/img/easy-admin.png")}}" type="image/png">
-
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    
+    <div class="row justify-content-center mt-5">
+        <div class="">
+                <h1 class="ml-5">Welcome to easy Admin</h1>
+                <img src="{{ asset("frontend/assets/img/easy-admin.png")}}" class="img-fluid" alt="...">
+        </div>
+    </div>
 </body>
 </html>
