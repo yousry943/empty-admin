8f16b08644ce5ff92c9802fa575a5a60a00a3650
jaime <jaime@Ubuntu.myguest.virtualbox.org>
2023-07-05 01:05:58 -0700

    adding login new view

--- resources/views/frontend/dashboard/user/auth/login.blade.php
+++ resources/views/frontend/dashboard/user/auth/login.blade.php
@@ -1,53 +1,64 @@
 <!DOCTYPE html>
-<html>
-
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="auto">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
-  <meta name="author" content="Creative Tim">
-  <title>{{ config('app.name') }} | Login</title>
-  <!-- Favicon -->
-  <link rel="icon" href="{{ asset("frontend/assets/img/easy-admin.png")}}" type="image/png">
-  <!-- Fonts -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
-  <!-- Icons -->
-  <link rel="stylesheet" href="{{ asset('frontend/assets/vendor/nucleo/css/nucleo.css') }}" type="text/css">
-  <link rel="stylesheet" href="{{ asset('frontend/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css') }}"
-    type="text/css">
-  <!-- Argon CSS -->
-  <link rel="stylesheet" href="{{ asset('frontend/assets/css/argon.css?v=1.2.0') }}" type="text/css">
-</head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<body class="my-bg">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link rel="icon" href="{{ asset("frontend/assets/img/easy-admin.png")}}" type="image/png">
+    <title>{{ config('app.name') }} | Login</title>
+
+    <!-- Scripts -->
+	  <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    @if(\Mcamara\LaravelLocalization\Facades\LaravelLocalization::getCurrentLocale()=='ar')
+    <style>
+      body{
+        direction: rtl;
+      }
+    </style>
+    @else
+    <style>
+      body{
+        direction: ltr;
+      }
+    </style>
+    @endif
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+
   <!-- Main content -->
   <div class="main-content">
     <!-- Header -->
-    <div class="header  py-7 py-lg-8 pt-lg-9">
 
-      <div class="separator separator-bottom separator-skew zindex-100">
-        <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1"
-          xmlns="http://www.w3.org/2000/svg">
-        </svg>
-      </div>
-    </div>
     <!-- Page content -->
-    <div class="container mt--8 pb-5">
+    <div class="container mx-0 mw-100">
       <div class="row justify-content-center">
-        <div class="col-lg-5 col-md-7">
-          <div class="card bg-secondary border-0 mb-0">
-            <div class="card-body px-lg-5 py-lg-5">
+
+        <div class="col-lg-6 col-md-6 mt-5">
+          <div class="text-center mt-5">
+            <img class="mt-5 img-fluid"src="{{ asset("frontend/assets/img/easy-admin.png")}}" alt="">
+          </div>
+          <div class="mt-0 mx-5 mb-5">
+            <div class=" px-lg-5 py-lg-5">
               <div class="text-center text-muted mb-4">
-                <h1>{{ config('app.name') }} | Login</h1>
+                {{-- <h1>{{ config('app.name') }} | Login</h1> --}}
               </div>
               <form method="POST" action="{{ route('dashboard.login') }}" aria-label="{{ __('Login') }}">
                 @csrf
                 <div class="form-group mb-3">
                   <div class="input-group input-group-merge input-group-alternative">
                     <div class="input-group-prepend">
-                      <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                      <span class=""><i class="ni ni-email-83"></i></span>
                     </div>
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                    <input id="email" type="email" placeholder="{{ __('Dashboard.Email') }}" class="form-control @error('email') is-invalid @enderror"
                       name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                     @error('email')
@@ -60,9 +71,9 @@
                 <div class="form-group">
                   <div class="input-group input-group-merge input-group-alternative">
                     <div class="input-group-prepend">
-                      <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                      <span class=""><i class="ni ni-lock-circle-open"></i></span>
                     </div>
-                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
+                    <input id="password" type="password" placeholder="{{ __('Dashboard.Password') }}" class="form-control @error('password') is-invalid @enderror"
                       name="password" required autocomplete="current-password">
                     @error('password')
                     <span class="invalid-feedback" role="alert">
@@ -75,17 +86,17 @@
                   <input class="custom-control-input" type="checkbox" name="remember" id="customCheckLogin"
                     {{ old('remember') ? 'checked' : '' }}>
                   <label class="custom-control-label" for="customCheckLogin">
-                    <span class="text-muted">Remember me</span>
+                    <span class="text-muted">{{ __('Dashboard.Remember me') }}</span>
                   </label>
                 </div>
                 <div class="mt-3">
                   <button type="submit" class="btn btn-primary">
-                    {{ __('Login') }}
+                    {{ __('Dashboard.Login') }}
                   </button>
 
                   @if (Route::has('dashboard.password.request'))
                   <a class="btn btn-link float-right" href="{{ route('dashboard.password.request') }}">
-                    {{ __('Forgot Your Password?') }}
+                    {{ __('Dashboard.Forgot Your Password?') }}
                   </a>
                   @endif
                 </div>
@@ -93,18 +104,29 @@
             </div>
           </div>
         </div>
+
+
+        <div class="col-lg-6 col-md-6  d-none d-sm-block " id="imgdiv" style="min-height: 100vh; background-position: center; background-size: cover; background-repeat: no-repeat; ">
+        </div>
       </div>
     </div>
   </div>
   <!-- Argon Scripts -->
   <!-- Core -->
-  <script src="{{ asset('frontend/assets/vendor/jquery/dist/jquery.min.js') }}"></script>
-  <script src="{{ asset('frontend/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ asset('frontend/assets/vendor/js-cookie/js.cookie.js') }}"></script>
-  <script src="{{ asset('frontend/assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js') }}"></script>
-  <script src="{{ asset('frontend/assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.j') }}s"></script>
-  <!-- Argon JS -->
-  <script src="{{ asset('frontend/assets/js/argon.js?v=1.2.0') }}"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      var urls = ['{{ asset('frontend/assets/img/bg1.jpg')}}', '{{ asset('frontend/assets/img/bg2.jpg')}}'];
+
+      var cout = 1;
+      $('#imgdiv').css('background-image', 'url("' + urls[0] + '")');
+      setInterval(function() {
+        $('#imgdiv').css('background-image', 'url("' + urls[cout] + '")');
+        cout == urls.length-1 ? cout = 0 : cout++;
+      }, 3000);
+
+    });
+  </script>
 </body>
 
 </html>
